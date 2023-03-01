d0260dd699dfad8e04e76029b7fc7ac78b2ce727
Prannaya <prannayagupta@gmail.com>
2023-03-01 16:58:14 +0800

    write dummy code I can't even test

--- server/src/main.py
+++ server/src/main.py
@@ -1,3 +1,19 @@
 from sandbox import sandbox
+from flask import Flask, request, jsonify
+
+from flask_cors import CORS
 
 print(sandbox.sandbox("print('hello world')"))
+
+app = Flask(__name__)
+app.secret_key = "super_secret_key"
+CORS(app)
+
+@app.route("/sendCode", methods=["POST"])
+def sendCode():
+    code = request.get_json()['code']
+    imports = "from troop import enemiesWithinRange, getFriendlyTroops, distanceToEntity\n"
+    return jsonify({"output": sandbox.sandbox(imports + code)});
+
+if __name__ == "__main__":
+    app.run(port=8080)
