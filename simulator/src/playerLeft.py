bd9eb5b81d0df864cf786b30e75161e57a2a9afa
Prannaya <prannayagupta@gmail.com>
2023-03-02 00:00:32 +0800

    simulator has been moved and stuff

---
+++ simulator/src/playerLeft.py
@@ -0,0 +1,11 @@
+from troop import enemiesWithinRange, getFriendlyTroops, distanceToEntity
+# Insert the import statements above programatically (user not supposed to write this)
+
+# Sample player code
+def distributeSkill(points):
+    return [points//4, points//4, points//4, points//4]
+
+def decideAction(self):
+    attackable = enemiesWithinRange(self)
+    if (len(attackable) != 0): self.attack(attackable[0])
+    else: self.move(1)
