a5878a9468932d91f61bcb2225e09dc44f6d3db6
Prannaya <prannayagupta@gmail.com>
2023-03-10 16:24:34 +0800

    remove the funny sendCodeHTML

--- server/src/main.py
+++ server/src/main.py
@@ -6,16 +6,9 @@ app = Flask(__name__)
 app.secret_key = "super_secret_key"
 CORS(app)
 
-
 @app.route("/<name>")
 def home(name):
     return f"<h1>Hello World, {name}</h1>"
-
-
-@app.route("/sendCodeHTML/<code>")
-def sendCodeHTML(code):
-    imports = "from troop import enemiesWithinRange, getFriendlyTroops, distanceToEntity\n\n"
-    return sandbox.sandbox(imports + code)[1]
 
 
 @app.route("/sendCode", methods=["POST"])
@@ -23,7 +16,6 @@ def sendCode():
     code = request.get_json()["code"]
     imports = "from troop import enemiesWithinRange, getFriendlyTroops, distanceToEntity\n\n"
     return jsonify(dict(zip(("status", "output"), sandbox.sandbox(imports + code))))
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
