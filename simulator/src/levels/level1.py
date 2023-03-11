612808b84b7ec6a4cde4382403cfbd188a49929e
Haloen <72333894+Haloen@users.noreply.github.com>
2023-03-11 22:35:44 +0800

    Level 1&2 is no longer a sitting duck

--- simulator/src/levels/level1.py
+++ simulator/src/levels/level1.py
@@ -1,8 +1,9 @@
 from gameAPI import enemiesWithinRange, getFriendlyTroops, distanceToEntity
 
-# AI for level 1 (Sitting Duck)
+# AI for level 1 (Stands still and defends)
 def distributeSkill(points):
-    return [0, 0, 0, 0]
+    return [1,1,1,1]
 
 def decideAction(self):
-    pass
+    attackable = enemiesWithinRange(self)
+    if (len(attackable) != 0): self.attack(attackable[0])
--- simulator/src/levels/level6.py
+++ simulator/src/levels/level6.py
@@ -1,7 +1,7 @@
 from gameAPI import enemiesWithinRange, getFriendlyTroops, distanceToEntity
 
 # AI for level 6 (High Range)
-# Beaten by level 3 AI, beats level 4,5 easily
+# Beaten by level 3 AI, beats level 4,5
 def distributeSkill(points):
     return [0, 0, points, 0]
 
@@ -10,6 +10,6 @@ def decideAction(self):
     enemies = enemiesWithinRange(self)
 
     # Attack any enemy within range
-    if (len(enemies) > 0): self.attack(enemies[-1])
+    if (len(enemies) > 0): self.attack(enemies[0]) # Attacks bases first
     else: self.move(1)
     
