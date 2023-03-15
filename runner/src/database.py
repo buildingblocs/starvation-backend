00e609f9a8d0476244ae390dd532d8780120aecc
squi-ddy <liuwenkai2017@gmail.com>
2023-03-15 23:54:22 +0800

    fix pfp type

--- runner/src/database.py
+++ runner/src/database.py
@@ -25,7 +25,7 @@ class Database:
                                 username varchar(255),
                                 school varchar(100),
                                 about varchar(255),
-                                pfp blob,
+                                pfp bloba,
                                 score int not null default 1000,
                                 code text,
                                 error_count int not null default 0,
