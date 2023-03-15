57f54785987a226860b14335c55cf18e9814623c
waffleSheep <zayan.superboy@gmail.com>
2023-03-15 22:01:28 +0800

    level 8 done

--- simulator/src/levels/level8.py
+++ simulator/src/levels/level8.py
@@ -1,8 +1,19 @@
 from gameAPI import enemiesWithinRange, getFriendlyTroops, distanceToEntity
 
-# AI for level 1 (Sitting Duck)
+# AI for level 8 (Alternating attack and heal)
+# Beaten by level 3 AI, beats level 4,5,6
 def distributeSkill(points):
-    return [0, 0, 0, 0]
+    return [0, 0, points, 0]
 
 def decideAction(self):
-    pass
+    global cnt
+    enemies = enemiesWithinRange(self)
+
+    # Attack any enemy within range
+    if (len(enemies) > 0): 
+        if(cnt % 2 == 0):
+            self.attack(enemies[-1]) # Now targets troops first
+        else:
+            self.update_health(5)
+    else: self.move(1)
+    
