b44735f0a26d8e4b86807ffca7a2329f64584ac4
squi-ddy <liuwenkai2017@gmail.com>
2023-03-17 01:11:36 +0800

    ...

--- server/src/main.py
+++ server/src/main.py
@@ -169,7 +169,7 @@ def callback():
     next_ = session.pop("next", "/")
 
     if not User.get(id):
-        if session["create"] != "true":
+        if session.pop("create", None) != "true":
             return redirect(next_)
         with open("src/default.png", "rb") as f:
             pfp = f.read()
