89973f6c6a8da1f094e779a0e0dc2d4caeaf87c5
squi-ddy <liuwenkai2017@gmail.com>
2023-03-17 06:09:55 +0800

    revert cors

--- server/src/main.py
+++ server/src/main.py
@@ -18,7 +18,7 @@ import arrow
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 db = Database()
-CORS(app, origins=["http://localhost:8080", "https://starvation.buildingblocs.sg"], supports_credentials=True)
+CORS(app)
 
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "")
